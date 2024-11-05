cbdd26bed89834521d11e69d51fa9dad0ec9d098
jhoewellposas <jhoewellposas@gmail.com>
2024-11-05 11:04:30 +0800

    working edit and delete

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', function () {
+/*Route::get('/home', function () {
     return view('home');
 });
+*/
 
 Route::get('/upload', function () {
     return view('upload'); // This view contains your HTML form
@@ -18,7 +19,14 @@ Route::get('/upload', function () {
 // Route to handle form submission (POST request only)
 Route::post('/extract', [CertificateController::class, 'extractCertificateData']);
 
+//edit and delete
+Route::post('/certificate/update/{id}', [CertificateController::class, 'updateCertificate'])->name('certificate.update');
+Route::delete('/certificate/delete/{id}', [CertificateController::class, 'deleteCertificate'])->name('certificate.delete');
+
+//index
+Route::get('/home', [CertificateController::class, 'showCertificates'])->name('home');
+
+//summary
 Route::get('/summary', function () {
     return view('summary');
 });
-
